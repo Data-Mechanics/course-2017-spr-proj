5eab1ffcd506d052bdefbb4eae200694b85d517f
Xiaotong Niu <Sylvia@Silnue-NiudeMacBook-Air.local>
2017-04-24 00:08:46 -0400

    poster part complete

--- pt0713_silnuext/poster/coordinates.py
+++ pt0713_silnuext/poster/coordinates.py
@@ -34,7 +34,7 @@ properties = {'marker_color': "#FOO",
 			    42.3211059996542
 			  ]
 			  }
-geometry = [-71.08318212371852, 42.32110599965421]
+geometry = geojson.Point([-71.08318212371852, 42.32110599965421])
 features.append(geojson.Feature(geometry=geometry, properties=properties))
 
 

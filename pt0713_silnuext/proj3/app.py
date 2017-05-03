70851813bb3f70b64c73a54e1ba975f7a746dad7
Po-Yu, Tseng <erictpoyu@gmail.com>
2017-05-03 18:40:39 -0400

    app 4 functions
    hi

--- pt0713_silnuext/proj3/app.py
+++ pt0713_silnuext/proj3/app.py
@@ -19,3 +19,37 @@ def get_crime_points():
 def get_optimization_point():
 	optimization_point = json.load(open('static/optimization.geojson', 'r'))
 	return optimization_point
+
+def list_crime():
+	crimes = get_crimes()['features']
+	crimes_list = [s['properties']['crime'] for s in crimes]
+	return render_template("map.html", crimes=crimes_list)
+
+def one_optimization():
+	optimization = get_crimes()['features']
+	optimization_list = [s['properties']['crime'] for s in optimization]
+	return render_template("map.html", optimization = optimization_list)
+
+def select_crime():
+	crime = request.form.get("crime")
+	routes = get_routes()['features']
+	selected_crime = []
+	for r in routes:
+		if r['properties']['crime'] == crime:
+			selected_crime.append(r)
+	print(selected_crime)
+	return render_template("map.html", route=selected_crime)
+
+def select_optimization():
+	optimization = request.form.get("optimization")
+	routes = get_routes()['features']
+	selected_optimization = []
+	for r in routes:
+		if r['properties']['optimization'] == optimization:
+			selected_optimization.append(r)
+	print(selected_optimization)
+	return render_template("map.html", route=selected_optimization)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

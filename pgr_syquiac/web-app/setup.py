0ee2629b5c42289b37d81a3e325b42acd798b4f7
Pauline Ramirez <paulineramirez@dhcp-wifi-8021x-155-41-45-46.bu.edu>
2017-05-04 20:12:45 -0400

    removed files

--- pgr_syquiac/web-app/setup.py
+++
@@ -1,8 +0,0 @@
-from retrieveData import retrieveData
-from transformation0 import transformation0
-
-# Retrieve data
-retrieveData.execute()
-
-# Run the first transformation
-transformation0.execute()

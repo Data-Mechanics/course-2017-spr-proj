a2820e28fcefb29a6c78401134f0772fe5b4d831
Ryan Chen <c.ryanchen@gmail.com>
2017-05-05 22:29:28 -0400

    fixed second 500 error, both visuals work on the server now

--- mrhoran_rnchen_vthomson/web_app/591_webvisual.py
+++ mrhoran_rnchen_vthomson/web_app/591_webvisual.py
@@ -1,8 +1,8 @@
 import json 
-from flask import Flask, jsonify, abort, make_response, request, render_template
+from flask import Flask, jsonify, abort, make_response, request, render_template, send_from_directory
 #from flask.ext.httpauth import HTTPBasicAuth
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 #auth = HTTPBasicAuth()
 
 @app.route('/')
@@ -18,9 +18,9 @@ def getSchoolHubs():
 	schoolHubs = json.load(open('../visualization/kmeans-visual/kmeans_hubs.json','r'))
         return jsonify(schoolHubs)
 
-#@app.route('/getCSV')
-#def getCSV():
-#	CSV = 
+@app.route('/getCSV')
+def getCSV():
+	return send_from_directory(app.static_folder, "kmeans_stats.csv")
 
 @app.route('/kmeans_cost_graph', methods=['GET'])
 def kmeans_cost_graph():

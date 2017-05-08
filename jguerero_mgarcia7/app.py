afa538c579c790f411ccde33656ce452ab78f9cb
velvetbleurose <jguerero@bu.edu>
2017-05-08 18:23:18 -0400

    adding correlation function that gets data values to plot correlation

--- jguerero_mgarcia7/app.py
+++ jguerero_mgarcia7/app.py
@@ -12,66 +12,66 @@ from bson.json_util import dumps
 
 app_name = Flask(__name__)
 
-class app(dml.Algorithm):
-	contributor = 'jguerero_mgarcia7'
-	reads = ['jguerero_mgarcia7.neighborhoodstatistics']
-	writes = []
+# class app(dml.Algorithm):
+# 	contributor = 'jguerero_mgarcia7'
+# 	reads = ['jguerero_mgarcia7.neighborhoodstatistics']
+# 	writes = []
 
 
-	@staticmethod
-	def execute(trial = False):
-		'''Retrieve some data sets (not using the API here for the sake of simplicity).'''
-		startTime = datetime.datetime.now()
+# 	@staticmethod
+# 	def execute(trial = False):
+# 		'''Retrieve some data sets (not using the API here for the sake of simplicity).'''
+# 		startTime = datetime.datetime.now()
 
-		app_name.run(host='0.0.0.0',port=5000,debug=True)
+# 		app_name.run(host='0.0.0.0',port=5000,debug=True)
 
-		endTime = datetime.datetime.now()
+# 		endTime = datetime.datetime.now()
 
-		return {"start":startTime, "end":endTime}
+# 		return {"start":startTime, "end":endTime}
 
-	@staticmethod
-	def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
-		'''
-			Create the provenance document describing everything happening
-			in this script. Each run of the script will generate a new
-			document describing that invocation event.
-			'''
+# 	@staticmethod
+# 	def provenance(doc = prov.model.ProvDocument(), startTime = None, endTime = None):
+# 		'''
+# 			Create the provenance document describing everything happening
+# 			in this script. Each run of the script will generate a new
+# 			document describing that invocation event.
+# 			'''
 
-		# Set up the database connection.
-		client = dml.pymongo.MongoClient()
-		repo = client.repo
-		repo.authenticate('jguerero_mgarcia7', 'jguerero_mgarcia7')
-		doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
-		doc.add_namespace('dat', 'http://datamechanics.io/data/jguerero_mgarcia7') # The data sets are in <user>#<collection> format.
-		doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
-		doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
+# 		# Set up the database connection.
+# 		client = dml.pymongo.MongoClient()
+# 		repo = client.repo
+# 		repo.authenticate('jguerero_mgarcia7', 'jguerero_mgarcia7')
+# 		doc.add_namespace('alg', 'http://datamechanics.io/algorithm/') # The scripts are in <folder>#<filename> format.
+# 		doc.add_namespace('dat', 'http://datamechanics.io/data/jguerero_mgarcia7') # The data sets are in <user>#<collection> format.
+# 		doc.add_namespace('ont', 'http://datamechanics.io/ontology#') # 'Extension', 'DataResource', 'DataSet', 'Retrieval', 'Query', or 'Computation'.
+# 		doc.add_namespace('log', 'http://datamechanics.io/log/') # The event log.
 
-		this_script = doc.agent('alg:jguerero_mgarcia7#app', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
+# 		this_script = doc.agent('alg:jguerero_mgarcia7#app', {prov.model.PROV_TYPE:prov.model.PROV['SoftwareAgent'], 'ont:Extension':'py'})
 		
-		neighborhoodstatistics_resource = doc.entity('dat:neighborhoodstatistics', {'prov:label':'Neighborhood Statistics', prov.model.PROV_TYPE:'ont:DataSet'})
+# 		neighborhoodstatistics_resource = doc.entity('dat:neighborhoodstatistics', {'prov:label':'Neighborhood Statistics', prov.model.PROV_TYPE:'ont:DataSet'})
 
 
-		get_visualizations = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
+# 		get_visualizations = doc.activity('log:uuid'+str(uuid.uuid4()), startTime, endTime)
 
-		doc.wasAssociatedWith(get_statistics, this_script)
-		doc.usage(get_statistics, neighborhoodstatistics_resource, startTime, None,
-				  {prov.model.PROV_TYPE:'ont:Computation'}
-				  )
+# 		doc.wasAssociatedWith(get_statistics, this_script)
+# 		doc.usage(get_statistics, neighborhoodstatistics_resource, startTime, None,
+# 				  {prov.model.PROV_TYPE:'ont:Computation'}
+# 				  )
  
-		app = doc.entity('dat:jguerero_mgarcia7#app', {prov.model.PROV_LABEL:'Visualization', prov.model.PROV_TYPE:'ont:DataSet'})
-		doc.wasAttributedTo(app, this_script)
-		doc.wasGeneratedBy(app, get_visualizations, endTime)
-		doc.wasDerivedFrom(app, neighborhoodstatistics_resource, get_visualizations, get_visualizations, get_visualizations)
+# 		app = doc.entity('dat:jguerero_mgarcia7#app', {prov.model.PROV_LABEL:'Visualization', prov.model.PROV_TYPE:'ont:DataSet'})
+# 		doc.wasAttributedTo(app, this_script)
+# 		doc.wasGeneratedBy(app, get_visualizations, endTime)
+# 		doc.wasDerivedFrom(app, neighborhoodstatistics_resource, get_visualizations, get_visualizations, get_visualizations)
 
 
-		repo.logout()
+# 		repo.logout()
 				  
-		return doc
+# 		return doc
 
 
 @app_name.route("/")
 def index():
-	return render_template("index.html")
+	return render_template("page.html")
 
 
 @app_name.route("/nbjson")
@@ -88,6 +88,7 @@ def nbstats():
 	FIELDS = {'name': True, 'the_geom': True, '_id': False}
 	neighborhoods = []
 	idx = 0
+
 	for n in repo['jguerero_mgarcia7.neighborhoods'].find(projection=FIELDS):
 		try:
 			pnb = stats[n["name"]]
@@ -110,7 +111,42 @@ def nbstats():
 	repo.logout()
 	return gj
 
+@app_name.route("/correlationjson")
+def correlationjson(x, y):
+	#return {x: , y:, name:''}
+
+	stats = json.loads(nbstats())
+	v = stats['features']
+#	print (stats)
+	
+	if x == 'income' and y == 'obesity':
+		complete = []
+		for i in v:
+			no = {'avg_num_food', 'dist_closest', 'quality_food'}
+			temp = {} 
+			dic = {key: val for key, val in i['properties'].items() if key not in no}
+		#	print (dic)
+			temp = {'x': 0, 'y': 0, 'name': 'empty'}
+			temp['x'] = dic['avg_income']
+			temp['y'] = dic['obesity']
+			temp['name'] = dic['name']
+			print (temp)
+			complete.append(temp)
 
 
-if __name__ == "__main__":
-	app_name.run(host='0.0.0.0',port=5000,debug=True)
+
+	# abc = {"type":"insecure","id":"1","name":"peter"}
+	# black_list = {"type"}
+	# rename ={"id":"identity"}  #use a mapping dictionary in case you want to rename multiple items
+	# dic = {rename.get(key,key) : val for key ,val in abc.items() if key not in black_list}
+	# print dic
+
+	return json.dumps(complete)
+
+#v = nbstats()
+v = other('income', 'obesity')
+print (v)
+
+
+# if __name__ == "__main__":
+# 	app_name.run(host='0.0.0.0',port=5000,debug=True)
